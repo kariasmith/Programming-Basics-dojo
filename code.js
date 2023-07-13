775809d51ae5d68ae1693c9f1493889872b1690c
Kari Smith <kariasmith@hotmail.com>
2023-07-13 01:03:11 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,7 @@
+// Creating a variable named likeCount and set to value of 0.
+var likeCount = 0;
+// Creating funtion named increaseLikes
+function increaseLikes() {
+    // Increasing the likeCount value by 1
+    likeCount = likeCount + 1;
+}
---
+++ code_Debug.js
@@ -0,0 +1,9 @@
+function main() {
+    function greeting(){
+        return "Hello World";
+    }
+    var word = greeting();
+    console.log(word);
+}
+
+main();
